5ac69883e245de4ba378d83dcc47b9713aa480ab
karthikalle <karthikalle@vagvlan536.0929.wlan.wireless-pennnet.upenn.edu>
2013-11-18 01:05:38 -0500

    Final Working... Testing started

---
+++ DataEntry/src/dataEntryTesting/UploadToSimpleDBTest.java
@@ -0,0 +1,22 @@
+package dataEntryTesting;
+
+import org.junit.Test;
+
+import dataEntry.GUI_DataEntry;
+import junit.framework.TestCase;
+
+public class UploadToSimpleDBTest extends TestCase {
+	
+	@Test
+	public void noImageName() {
+		GUI_DataEntry gde = new GUI_DataEntry();
+		
+		gde.word="";
+		gde.
+	}
+	
+	
+	public UploadToSimpleDBTest(String name) {
+		super(name);
+	}
+}
